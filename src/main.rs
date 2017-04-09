b066dfb07b62a006d8bb6c057db6b6335c4a7903
Gianmarco <gianmarcoesgarix@gmail.com>
2017-04-09 22:39:21 +0200

    GUI designing

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,9 @@ extern crate lcs;
 extern crate gtk;
 
 use gtk::prelude::*;
-use gtk::{ToolButton, SpinButton, MessageType, Dialog, Window, Box};
+use gtk::{ToolButton, SpinButton, Button, Dialog, Window, Box};
 use gtk::{Builder, ApplicationWindow, MenuItem, AboutDialog};
-use gtk::{Label, Entry, ComboBoxText, Orientation};
+use gtk::{Label, Entry, ComboBoxText, Orientation, Statusbar};
 
 use lcs::dmxsystem::universe::Universe;
 
@@ -38,57 +38,48 @@ fn main() {
         panic!("Fatal error! Unable to initialize Graphic Interface")
     }
 
-    let builder = Builder::new_from_file("GUI.glade");
+    let glade_src = include_str!("GUI.glade");
+
+    let builder = Builder::new_from_string(glade_src);
     let window = builder.get_object::<ApplicationWindow>("main").unwrap();
-    let mut tmp_menuitem = builder.get_object::<MenuItem>("about_menu").unwrap();
-    let mut tmp_dialog = builder.get_object::<Dialog>("about").unwrap();
-    let mut tmp_button: ToolButton = builder.get_object("add_light_btn").unwrap();
-        
+    let tmp_menuitem = builder.get_object::<MenuItem>("about_menu").unwrap();
+    let tmp_dialog = builder.get_object::<Dialog>("about").unwrap();
+    let tmp_d = tmp_dialog.clone();
+    tmp_dialog.connect_close(move |_|{
+        tmp_d.hide();
+    });
+    let tmp_button: ToolButton = builder.get_object("add_light_btn").unwrap();
+    
     tmp_menuitem.connect_activate(move |_| {
         tmp_dialog.show();
     });
     
-    tmp_dialog = builder.get_object("add_light_dialog").unwrap();
+    let tmp_dialog = builder.get_object::<Dialog>("add_light_dialog").unwrap();
+    let tmp_d:Dialog = tmp_dialog.clone();
     tmp_button.connect_clicked(move |_| {
-        tmp_dialog.show();
+        tmp_d.show();
+    });
+    let tmp_d = tmp_dialog.clone();
+    builder.get_object::<Button>("add_light_cancel").unwrap().connect_clicked(move |_| {
+        tmp_d.hide();
+    });
+    
+    builder.get_object::<Button>("add_light_ok").unwrap().connect_clicked(move |_| {
+        //add_light();
+        tmp_dialog.hide();
     });
     //after the first time the button gets broken!
-    let bx = builder.get_object::<gtk::Box>("add_light_box").unwrap();
-    //let proto = builder.get_object::<gtk::Box>("prototype").unwrap();
-    builder.get_object::<SpinButton>("num_ch").unwrap().connect_value_changed(move |b| {
-        let value = b.get_value_as_int();
-        let i = bx.get_children().len() - 3;
-        if value < i as i32 {
-            //remove last element
-        } else {
-            //add element
-            let strng = format!("Channel {}", i+1);
-            let lbl = Label::new(Some(strng.as_str()));
-            let txt = Entry::new();
-            let cmb = ComboBoxText::new();
-            cmb.append(None, "Dimmer");
-            cmb.append(None, "Red");
-            cmb.append(None, "Green");
-            cmb.append(None, "Blue");
-            cmb.append(None, "None");
-            let mut x = Box::new(Orientation::Horizontal, 0);
-            x.pack_end(&lbl, false, false, 0);
-            x.pack_end(&txt, false, false, 0);
-            x.pack_end(&cmb, false, false, 0);
 
-            bx.pack_end(&x, false, false, 0);
-        }
-    });
-
-    tmp_menuitem = builder.get_object("quit_menuitem").unwrap();
+    let tmp_menuitem: MenuItem = builder.get_object("quit_menuitem").unwrap();
     tmp_menuitem.connect_activate(|_| {
         gtk::main_quit();
     });
 
-    tmp_dialog = builder.get_object::<Dialog>("welcome").unwrap();
+    let tmp_dialog = builder.get_object::<Dialog>("welcome").unwrap();
     tmp_dialog.show();
+    let tmp_d = tmp_dialog.clone();
     tmp_dialog.connect_close(move |_| {
-        //tmp_dialog.destroy(); //error: cannot move `tmp_dialog` into closure because it is borrowed [E0504]
+        tmp_d.destroy();
     });
     
     window.connect_delete_event(|_,_| {
@@ -97,6 +88,9 @@ fn main() {
     });
     window.show_all();
 
+    let stat = builder.get_object::<Statusbar>("stat").unwrap();
+    let contid = stat.get_context_id("Ready");
+    stat.push(contid, "Ready");
     gtk::main();
 
     //let mut u = Universe::new();
